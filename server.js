cd6e10a249da1985f7b9c78b640bf72ceee89a5d
Espinosa19 <113320619+Espinosa19@users.noreply.github.com>
2026-04-16 00:00:55 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 import dotenv from 'dotenv';
 import app from './src/app.js';
 
+import simularConversaciones from './src/services/simularConversacion.js';
+
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
+  // Ejecutar simulación de conversaciones al iniciar
+  simularConversaciones().catch(e => console.error('Error en simulación:', e));
 });
