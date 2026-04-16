8c3c80bcec18ca6a624702966ee09bf1d3cc9fde
Espinosa19 <113320619+Espinosa19@users.noreply.github.com>
2026-04-16 00:02:16 -0600

    Delete src directory

--- src/routes/chat.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { chatWithAI } from '../controllers/chat.controller.js';
-
-const router = Router();
-
-// API interna para pruebas
-router.post('/', chatWithAI);
-
-export default router;
